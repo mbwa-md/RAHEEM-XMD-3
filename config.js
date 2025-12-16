8043bcd90125c46cef12d71ee749e95ad0870f7d
mbwa-md <fundimbwa@gmail.com>
2025-12-16 21:13:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>7ghQhTiZ#r6McofrJnRsTB1o7KQ9kcBIqXeGhbY6j22bP6tqEvSA",
+SESSION_ID: process.env.SESSION_ID || "6Ml1WYjI#JG2Ns5a8DZuF7dnrw8U27Q-_9xxd25RMVF_kQ3DcEMs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,13 +13,13 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*sᴇᴇɴ ʏᴏᴜʀ sᴛᴀᴛᴜs RAHEEM-CM🐺*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*sᴇᴇɴ ʏᴏᴜʀ sᴛᴀᴛᴜs 𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿🐺*",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "false",
+WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "true",
 // make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/9gl0l8.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "RAHEEM-XMD-3",
+BOT_NAME: process.env.BOT_NAME || "𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "RAHEEM-CM",
+STICKER_NAME: process.env.STICKER_NAME || "𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,21 +37,21 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255789661031",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "RAHEEM-CM",
+OWNER_NAME: process.env.OWNER_NAME || "𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ RAHEEM-CM*",
+DESCRIPTION: process.env.DESCRIPTION || "*© 𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/a97zm1.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> *RAHEEM-XM-3 always online❄️",
+LIVE_MSG: process.env.LIVE_MSG || "> *𝙎𝙄𝙇𝘼𝙏𝙍𝙄𝙓-𝙈𝘿 always online❄️",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
+ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -61,22 +61,22 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "true",
 // false if want mark commands as read 
-DEV: process.env.DEV || "255763111390",
+DEV: process.env.DEV || "255789661031",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };

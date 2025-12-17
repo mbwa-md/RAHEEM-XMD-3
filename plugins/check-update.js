50d303058ee72e860fb64ebc4dd0afaab3c85045
mbwa-md <fundimbwa@gmail.com>
2025-12-17 09:11:08 +0300

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -27,7 +27,7 @@ cmd({
     }
 
     // Fetch latest version data from GitHub
-    const rawVersionUrl = 'https://raw.githubusercontent.com/Raheem-cm/RAHEEM-XMD-3/main/data/version.json';
+    const rawVersionUrl = 'https://raw.githubusercontent.com/mbwa-md/RAHEEM-XMD-3/main/data/version.json';
     let latestVersion = 'Unknown';
     let latestChangelog = 'No changelog available.';
     try {
@@ -53,12 +53,12 @@ cmd({
     const lastUpdate = fs.statSync(localVersionPath).mtime.toLocaleString();
 
     // GitHub stats
-    const githubRepo = 'https://github.com/Raheem-cm/RAHEEM-XMD-3';
+    const githubRepo = 'https://github.com/mbwa-md/RAHEEM-XMD-3';
 
     // Check update status
-    let updateMessage = `✅ Your RAHEEM-XMD-3 bot is up-to-date!`;
+    let updateMessage = `✅ Your SILA-MD bot is up-to-date!`;
     if (localVersion !== latestVersion) {
-      updateMessage = `🚀 Your RAHEEM-XMD-3 bot is outdated!
+      updateMessage = `🚀 Your SILA-MD bot is outdated!
 🔹 *Current Version:* ${localVersion}
 🔹 *Latest Version:* ${latestVersion}
 
@@ -66,22 +66,22 @@ Use *.update* to update.`;
     }
 
     const statusMessage = `🌟 *Good ${new Date().getHours() < 12 ? 'Morning' : 'Night'}, ${pushname}!* 🌟\n\n` +
-      `📌 *Bot Name:* RAHEEM-XMD-3\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
+      `📌 *Bot Name:* SILA-MD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
       `💾 *System Info:*\n⏳ *Uptime:* ${uptime}\n📟 *RAM Usage:* ${ramUsage}MB / ${totalRam}MB\n⚙️ *Host Name:* ${hostName}\n📅 *Last Update:* ${lastUpdate}\n\n` +
       `📝 *Changelog:*\n${latestChangelog}\n\n` +
-      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [Raheem-cm](https://github.com/Raheem-cm)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
+      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [Sila-Md](https://github.com/mbwa-md)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
 
     // Send the status message with an image
     await conn.sendMessage(from, {
-      image: { url: 'https://files.catbox.moe/a97zm1.jpg' },
+      image: { url: 'https://files.catbox.moe/dlvrav.jpg' },
       caption: statusMessage,
       contextInfo: {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363399470975987@newsletter',
-          newsletterName: 'RAHEEM-XMD-3',
+          newsletterJid: '120363402325089913@newsletter',
+          newsletterName: '🎅𝚂𝙸𝙻𝙰 𝚃𝙴𝙲𝙷🎅',
           serverMessageId: 143
         }
       }
